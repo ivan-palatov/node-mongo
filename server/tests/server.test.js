ee8649ceb43b3cd2035bdaf9410b8b6bc808d9c1
Ivan <ivanakazver3d@gmail.com>
2018-07-20 00:56:44 +0700

    Added tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,15 @@ const request = require('supertest')
 const { app } = require('../server')
 const { Todo } = require('../models/todo')
 
+const Todos = [
+    { text: 'First test todo'},
+    { text: 'Second test todo'}
+]
+
 beforeEach(done => {
-    Todo.remove({}).then(() => done())
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(Todos)
+    }).then(() => done())
 })
 
 describe('POST /todos', () => {
@@ -22,7 +29,7 @@ describe('POST /todos', () => {
                 if (err) {
                     return done(err)
                 }
-                Todo.find()
+                Todo.find({text})
                     .then(todos => {
                         expect(todos.length).toBe(1)
                         expect(todos[0].text).toBe(text)
@@ -43,10 +50,22 @@ describe('POST /todos', () => {
                 }
                 Todo.find()
                     .then(todos => {
-                        expect(todos.length).toBe(0)
+                        expect(todos.length).toBe(2)
                         done()
                     })
                     .catch(err => done(err))
             })
+    })
+})
+
+describe('GET /todos', () => {
+    it('Should get all todos', done => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done)
     })
 })
