3566c8356eac2ce17b2bee31fa87159411e6f4bf
Ivan <ivanakazver3d@gmail.com>
2018-07-20 23:26:09 +0700

    Added password hashing

--- pg/hashing.js
+++ pg/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-const data = {
-    id: 10
-}
+const pw = '123!abvg'
 
-const token = jwt.sign(data, '123abc')
-console.log(token)
-const decoded = jwt.verify(token, '123abc')
-console.log(decoded)
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(pw, salt, (err, hash) => {
+//         console.log(hash)
+//     })
+// })
+
+const hashedPw = '$2a$10$Bv0GmP1bMluaJxCF0nGqKu4mnteKOHImz6wKi6BZC0th00OSKpiJ6'
+
+bcrypt.compare(pw, hashedPw, (err, res) => {
+    console.log(res)
+})
+
+// const data = {
+//     id: 10
+// }
+
+// const token = jwt.sign(data, '123abc')
+// console.log(token)
+// const decoded = jwt.verify(token, '123abc')
+// console.log(decoded)
 
 // const message = "Some string"
 // const sha = SHA256(message).toString()
