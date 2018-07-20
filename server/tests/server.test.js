ed098d8de1cae1decad1b47bdd3afb83f3b20a95
Ivan <ivanakazver3d@gmail.com>
2018-07-20 17:27:22 +0700

    Added tests to DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -95,3 +95,38 @@ describe('GET /todos/:id', () => {
             .end(done)
     })
 })
+
+describe('DELETE /todos/:id', () => {
+    it('Should remove a todo', done => {
+        const hexID = Todos[1]._id.toHexString()
+        request(app)
+            .delete(`/todos/${hexID}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo._id).toBe(hexID)
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+                Todo.findById(hexID)
+                    .then(todo => {
+                        expect(todo).toNotExist()
+                        done()
+                    })
+                    .catch(err => done(err))
+            })
+    })
+    it('Should return 404 if todo not found', done => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done)
+    })
+    it('Should return 404 if ObjectID is invalid', done => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .end(done)
+    })
+})
